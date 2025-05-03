64d1e4fda5a7ac98a18ad1f55a51311bebaa9211
sudeepa-sri <sudeepasri13@gmail.com>
2025-05-03 23:39:09 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Ensures Vite listens on all network interfaces
+    allowedHosts: [
+      'bogantiles.onrender.com', // Add your Render URL here
+      'localhost', // Optionally allow localhost for local development
+    ],
+  },
 })
