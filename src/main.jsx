60cbbc70383ed6335ee922dc355c956784d694d2
sudeepa-sri <sudeepasri13@gmail.com>
2025-05-04 17:17:50 +0530

    bootstrap updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-
-// Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import Bootstrap JS (for interactive components like dropdowns, modals, navbar toggles)
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-// Optional: If you're using Bootstrap Icons
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
